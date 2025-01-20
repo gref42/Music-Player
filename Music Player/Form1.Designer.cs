ba05587b1c6a24e432c888fcb73b240f3044ef22
G <141026526+gref42@users.noreply.github.com>
2025-01-20 10:29:56 +0200

    Add UI components and basic audio functionality

--- Music Player/Form1.Designer.cs
+++ Music Player/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            volumeSlider = new NAudio.Gui.VolumeSlider();
+            addSong_button = new Button();
+            play_button = new Button();
+            SuspendLayout();
+            // 
+            // volumeSlider
+            // 
+            volumeSlider.Location = new Point(415, 289);
+            volumeSlider.Name = "volumeSlider";
+            volumeSlider.Size = new Size(96, 16);
+            volumeSlider.TabIndex = 0;
+            // 
+            // addSong_button
+            // 
+            addSong_button.Location = new Point(157, 225);
+            addSong_button.Name = "addSong_button";
+            addSong_button.Size = new Size(75, 23);
+            addSong_button.TabIndex = 1;
+            addSong_button.Text = "Add Song";
+            addSong_button.UseVisualStyleBackColor = true;
+            addSong_button.Click += addSong_button_Click;
+            // 
+            // play_button
+            // 
+            play_button.Location = new Point(332, 157);
+            play_button.Name = "play_button";
+            play_button.Size = new Size(75, 23);
+            play_button.TabIndex = 2;
+            play_button.Text = "Play";
+            play_button.UseVisualStyleBackColor = true;
+            play_button.Click += play_button_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(play_button);
+            Controls.Add(addSong_button);
+            Controls.Add(volumeSlider);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private NAudio.Gui.VolumeSlider volumeSlider;
+        private Button addSong_button;
+        private Button play_button;
     }
 }
--- Music Player/Form1.cs
+++ Music Player/Form1.cs
@@ -1,10 +1,56 @@
+using NAudio.Wave;
+
 namespace Music_Player
 {
     public partial class Form1 : Form
     {
+        private List<string> songPaths;
+
+        private IWavePlayer musicPlayer;
+        private AudioFileReader audioFile;
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            songPaths = new List<string>();
+
+        }
+
+        private void addSong_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Select a song",
+                Filter = "Audio files|*.mp3;*.wav;*.flac;*.aac",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic),
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show($"You have selected: {openFileDialog.FileName}", "Song selected");
+                songPaths.Add(openFileDialog.FileName);
+            }
+            else
+            {
+                MessageBox.Show("You haven't selected a song", "No selection");
+            }
+        }
+
+        private void play_button_Click(object sender, EventArgs e)
+        {
+            string filePath = $"{songPaths[songPaths.Count - 1]}";
+
+            musicPlayer = new WaveOutEvent();
+            audioFile = new AudioFileReader(filePath);
+
+            musicPlayer.Init(audioFile);
+
+            musicPlayer.Play();
         }
     }
 }
