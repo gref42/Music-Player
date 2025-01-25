e8902a721180e37a7a27ef7ed19fefc381aa5912
G <141026526+gref42@users.noreply.github.com>
2025-01-26 01:17:19 +0200

    Refactor UI and add Song class for better management
    Updated variable names for UI elements for readability and consistency. Added new UI elements including columns for listViewSongs and a new button btnAddFolder. Adjusted layout and positioning of UI elements, including volumeSlider. Renamed and updated event handlers to handle the new Song class. Replaced songPaths list with songList storing Song instances. Introduced Song class to encapsulate song details. Updated btnPlayPause_Click to use Song class and handle song selection. Added btnAddFolder_Click for adding multiple songs from a folder. Simplified volumeSlider_ValueChanged method. Removed unused namespaces from Form1.cs. Added currentlyPlayingIndex variable. Partially updated btnNextSong_Click and btnPreviousSong_Click with TODO for previous song functionality.

--- Music Player/Form1.Designer.cs
+++ Music Player/Form1.Designer.cs
@@ -29,15 +29,19 @@
         private void InitializeComponent()
         {
             volumeSlider = new NAudio.Gui.VolumeSlider();
-            addSong_button = new Button();
+            btnAddSong = new Button();
             btnPlayPause = new Button();
-            listView = new ListView();
+            listViewSongs = new ListView();
+            Title = new ColumnHeader();
+            Author = new ColumnHeader();
+            Duration = new ColumnHeader();
             pnlCurrentSongDetails = new Panel();
             lblCurrentAuthor = new Label();
             lblCurrentSong = new Label();
             lblNowPlaying = new Label();
             btnPreviousSong = new Button();
             btnNextSong = new Button();
+            btnAddFolder = new Button();
             pnlCurrentSongDetails.SuspendLayout();
             SuspendLayout();
             // 
@@ -46,25 +50,25 @@
             volumeSlider.BackColor = SystemColors.ActiveCaption;
             volumeSlider.BackgroundImageLayout = ImageLayout.None;
             volumeSlider.BorderStyle = BorderStyle.FixedSingle;
-            volumeSlider.Location = new Point(352, 292);
+            volumeSlider.Location = new Point(471, 374);
             volumeSlider.Name = "volumeSlider";
             volumeSlider.Size = new Size(96, 16);
             volumeSlider.TabIndex = 0;
             volumeSlider.VolumeChanged += volumeSlider_ValueChanged;
             // 
-            // addSong_button
+            // btnAddSong
             // 
-            addSong_button.Location = new Point(241, 88);
-            addSong_button.Name = "addSong_button";
-            addSong_button.Size = new Size(75, 23);
-            addSong_button.TabIndex = 1;
-            addSong_button.Text = "Add Song";
-            addSong_button.UseVisualStyleBackColor = true;
-            addSong_button.Click += addSong_button_Click;
+            btnAddSong.Location = new Point(376, 325);
+            btnAddSong.Name = "btnAddSong";
+            btnAddSong.Size = new Size(75, 23);
+            btnAddSong.TabIndex = 1;
+            btnAddSong.Text = "Add Song";
+            btnAddSong.UseVisualStyleBackColor = true;
+            btnAddSong.Click += btnAddSong_Click;
             // 
             // btnPlayPause
             // 
-            btnPlayPause.Location = new Point(352, 88);
+            btnPlayPause.Location = new Point(477, 314);
             btnPlayPause.Name = "btnPlayPause";
             btnPlayPause.Size = new Size(75, 23);
             btnPlayPause.TabIndex = 2;
@@ -72,23 +76,41 @@
             btnPlayPause.UseVisualStyleBackColor = true;
             btnPlayPause.Click += btnPlayPause_Click;
             // 
-            // listView
+            // listViewSongs
             // 
-            listView.Location = new Point(31, 29);
-            listView.Margin = new Padding(3, 2, 3, 2);
-            listView.Name = "listView";
-            listView.Size = new Size(133, 279);
-            listView.TabIndex = 3;
-            listView.UseCompatibleStateImageBehavior = false;
+            listViewSongs.Columns.AddRange(new ColumnHeader[] { Title, Author, Duration });
+            listViewSongs.Location = new Point(31, 39);
+            listViewSongs.Margin = new Padding(3, 2, 3, 2);
+            listViewSongs.Name = "listViewSongs";
+            listViewSongs.Size = new Size(267, 269);
+            listViewSongs.TabIndex = 3;
+            listViewSongs.UseCompatibleStateImageBehavior = false;
+            listViewSongs.View = View.Details;
+            // 
+            // Title
+            // 
+            Title.Text = "Title";
+            Title.Width = 80;
+            // 
+            // Author
+            // 
+            Author.Text = "Author";
+            Author.TextAlign = HorizontalAlignment.Center;
+            Author.Width = 120;
+            // 
+            // Duration
+            // 
+            Duration.Text = "Duration";
+            Duration.TextAlign = HorizontalAlignment.Center;
             // 
             // pnlCurrentSongDetails
             // 
             pnlCurrentSongDetails.Controls.Add(lblCurrentAuthor);
             pnlCurrentSongDetails.Controls.Add(lblCurrentSong);
             pnlCurrentSongDetails.Controls.Add(lblNowPlaying);
-            pnlCurrentSongDetails.Location = new Point(227, 152);
+            pnlCurrentSongDetails.Location = new Point(352, 158);
             pnlCurrentSongDetails.Name = "pnlCurrentSongDetails";
-            pnlCurrentSongDetails.Size = new Size(168, 101);
+            pnlCurrentSongDetails.Size = new Size(200, 100);
             pnlCurrentSongDetails.TabIndex = 4;
             pnlCurrentSongDetails.Visible = false;
             // 
@@ -97,18 +119,18 @@
             lblCurrentAuthor.AutoSize = true;
             lblCurrentAuthor.Location = new Point(3, 41);
             lblCurrentAuthor.Name = "lblCurrentAuthor";
-            lblCurrentAuthor.Size = new Size(36, 15);
+            lblCurrentAuthor.Size = new Size(44, 15);
             lblCurrentAuthor.TabIndex = 2;
-            lblCurrentAuthor.Text = "None";
+            lblCurrentAuthor.Text = "Author";
             // 
             // lblCurrentSong
             // 
             lblCurrentSong.AutoSize = true;
             lblCurrentSong.Location = new Point(3, 26);
             lblCurrentSong.Name = "lblCurrentSong";
-            lblCurrentSong.Size = new Size(36, 15);
+            lblCurrentSong.Size = new Size(34, 15);
             lblCurrentSong.TabIndex = 1;
-            lblCurrentSong.Text = "None";
+            lblCurrentSong.Text = "Song";
             // 
             // lblNowPlaying
             // 
@@ -121,7 +143,7 @@
             // 
             // btnPreviousSong
             // 
-            btnPreviousSong.Location = new Point(207, 297);
+            btnPreviousSong.Location = new Point(376, 264);
             btnPreviousSong.Name = "btnPreviousSong";
             btnPreviousSong.Size = new Size(75, 23);
             btnPreviousSong.TabIndex = 5;
@@ -131,7 +153,7 @@
             // 
             // btnNextSong
             // 
-            btnNextSong.Location = new Point(220, 335);
+            btnNextSong.Location = new Point(471, 285);
             btnNextSong.Name = "btnNextSong";
             btnNextSong.Size = new Size(75, 23);
             btnNextSong.TabIndex = 6;
@@ -139,18 +161,29 @@
             btnNextSong.UseVisualStyleBackColor = true;
             btnNextSong.Click += btnNextSong_Click;
             // 
+            // btnAddFolder
+            // 
+            btnAddFolder.Location = new Point(237, 358);
+            btnAddFolder.Name = "btnAddFolder";
+            btnAddFolder.Size = new Size(75, 23);
+            btnAddFolder.TabIndex = 7;
+            btnAddFolder.Text = "Add Folder";
+            btnAddFolder.UseVisualStyleBackColor = true;
+            btnAddFolder.Click += btnAddFolder_Click;
+            // 
             // MusicPlayer
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientActiveCaption;
             ClientSize = new Size(597, 415);
+            Controls.Add(btnAddFolder);
             Controls.Add(btnNextSong);
             Controls.Add(btnPreviousSong);
             Controls.Add(pnlCurrentSongDetails);
-            Controls.Add(listView);
+            Controls.Add(listViewSongs);
             Controls.Add(btnPlayPause);
-            Controls.Add(addSong_button);
+            Controls.Add(btnAddSong);
             Controls.Add(volumeSlider);
             MinimumSize = new Size(440, 310);
             Name = "MusicPlayer";
@@ -164,14 +197,18 @@
         #endregion
 
         private NAudio.Gui.VolumeSlider volumeSlider;
-        private Button addSong_button;
+        private Button btnAddSong;
         private Button btnPlayPause;
-        private ListView listView;
+        private ListView listViewSongs;
         private Panel pnlCurrentSongDetails;
         private Label lblNowPlaying;
         private Label lblCurrentAuthor;
         private Label lblCurrentSong;
         private Button btnPreviousSong;
         private Button btnNextSong;
+        private ColumnHeader Title;
+        private ColumnHeader Author;
+        private ColumnHeader Duration;
+        private Button btnAddFolder;
     }
 }
--- Music Player/Form1.cs
+++ Music Player/Form1.cs
@@ -1,25 +1,24 @@
-using System.Diagnostics.CodeAnalysis;
-using System.DirectoryServices.ActiveDirectory;
 using NAudio.Wave;
 
 namespace Music_Player
 {
     public partial class MusicPlayer : Form
     {
-        private List<string> songPaths;
+        private List<Song> songList;
 
         private WaveOutEvent waveOut;
         private AudioFileReader? audioFileReader;
 
         private bool hasStarted = false;
         private bool isPlaying = false;
+        private int currentlyPlayingIndex = -1;
 
         private LinkedList<string> songHistory;
 
         public MusicPlayer()
         {
             InitializeComponent();
-            songPaths = new List<string>();
+            songList = new List<Song>();
             waveOut = new WaveOutEvent();
             songHistory = new LinkedList<string>();
         }
@@ -29,7 +28,7 @@ namespace Music_Player
 
         }
 
-        private void addSong_button_Click(object sender, EventArgs e)
+        private void btnAddSong_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
@@ -42,7 +41,11 @@ namespace Music_Player
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 //MessageBox.Show($"You have selected: {openFileDialog.FileName}", "Song selected");
-                songPaths.Add(openFileDialog.FileName);
+                var newSong = new Song(openFileDialog.FileName);
+                songList.Add(newSong);
+
+                var item = new ListViewItem([newSong.Title, newSong.Artist, newSong.Length.ToString(@"mm\:ss")]);
+                listViewSongs.Items.Add(item);
             }
             else
             {
@@ -52,23 +55,34 @@ namespace Music_Player
 
         private void btnPlayPause_Click(object sender, EventArgs e)
         {
-            if (!hasStarted)
+            int selectedSong;
+            try
+            {
+                selectedSong = listViewSongs.SelectedItems[0].Index;
+            }
+            catch (Exception exception)
+            {
+                if (hasStarted)
+                    selectedSong = currentlyPlayingIndex;
+                else
+                    selectedSong = 0;
+            }
+
+            if (!hasStarted || currentlyPlayingIndex != selectedSong)
             {
                 hasStarted = true;
 
-                string filePath = $"{songPaths[songPaths.Count - 1]}";
-
                 waveOut = new WaveOutEvent();
-                audioFileReader = new AudioFileReader(filePath);
+                audioFileReader = new AudioFileReader(songList[selectedSong].FilePath);
 
-                string[] meta = Path.GetFileNameWithoutExtension(filePath).Split('-');
 
-                lblCurrentSong.Text = meta.Length > 1 ? meta[1].Trim() : meta[0].Trim();
-                lblCurrentAuthor.Text = meta.Length > 1 ? meta[0].Trim() : "Unknown Author";
+                lblCurrentSong.Text = songList[selectedSong].Title;
+                lblCurrentAuthor.Text = songList[selectedSong].Artist;
 
                 pnlCurrentSongDetails.Visible = true;
 
                 waveOut.Init(audioFileReader);
+                waveOut.Volume = volumeSlider.Volume;
             }
 
 
@@ -91,15 +105,12 @@ namespace Music_Player
 
         private void volumeSlider_ValueChanged(object sender, EventArgs e)
         {
-            if (waveOut != null && waveOut.PlaybackState == PlaybackState.Playing)
-            {
-                waveOut.Volume = volumeSlider.Volume;
-            }
+            waveOut.Volume = volumeSlider.Volume;
         }
 
         private void btnNextSong_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btnPreviousSong_Click(object sender, EventArgs e)
@@ -114,5 +125,71 @@ namespace Music_Player
                 songHistory.RemoveLast();
             }
         }
+
+        private void btnAddFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog openFolderDialog = new FolderBrowserDialog()
+            {
+                Description = "Select a folder containing audio files",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic),
+                Multiselect = true
+            };
+
+            if (openFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = openFolderDialog.SelectedPath;
+
+                var audioFilesPath = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
+                    .Where(file =>
+                        file.EndsWith(".mp3") || file.EndsWith(".wav") || file.EndsWith(".flac") ||
+                        file.EndsWith(".aac"));
+
+                //MessageBox.Show($"You have selected: {openFileDialog.FileName}", "Song selected");
+                foreach (var path in audioFilesPath)
+                {
+                    var newSong = new Song(path);
+                    songList.Add(newSong);
+
+                    var item = new ListViewItem([newSong.Title, newSong.Artist, newSong.Length.ToString(@"mm\:ss")]);
+                    listViewSongs.Items.Add(item);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You haven't selected a folder", "No selection");
+            }
+        }
+    }
+
+    public class Song
+    {
+        public string Title;
+        public string Artist;
+        public TimeSpan Length;
+        public string FilePath;
+
+        public Song(string filePath)
+        {
+            FilePath = filePath;
+
+            try
+            {
+                using (var audioFile = new AudioFileReader(filePath))
+                {
+                    Length = audioFile.TotalTime;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{filePath} could not be added.", "Error loading file.");
+            }
+            
+
+            string[] meta = Path.GetFileNameWithoutExtension(filePath).Split('-');
+
+            Title = meta.Length > 1 ? meta[1].Trim() : meta[0].Trim();
+            Artist = meta.Length > 1 ? meta[0].Trim() : "Unknown Author";
+        }
+
     }
 }
